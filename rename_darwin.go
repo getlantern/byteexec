00a30049d0b3930b56bdcc1ba3085077c407b639
Harry Harpham <harry@getlantern.org>
2021-12-09 15:51:47 -0700

    Ensure proper config dir is used on macOS

--- rename_darwin.go
+++ rename_darwin.go
@@ -2,10 +2,23 @@
 
 package byteexec
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 func renameExecutable(orig string) string {
 	return orig
 }
 
 func pathForRelativeFiles() (string, error) {
-	return inHomeDir("Library/Application Support/byteexec")
+	// Use os.UserConfigDir to get to the "Application Support" directory. This is important as we
+	// may be running in a sandbox, in which case we need the "Application Support" directory in the
+	// container sandbox. os.UserConfigDir will return the correct result.
+	cfgDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", fmt.Errorf("failed to obtain user config directory: %w", err)
+	}
+	return filepath.Join(cfgDir, "byteexec"), nil
 }
